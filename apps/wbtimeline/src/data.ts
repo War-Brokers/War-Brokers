7e261aae5bfd737ff3ea64130a1f7bb640e61983
developomp <developomp@gmail.com>
2023-08-23 19:27:42 +0900

    refactor(wbtimeline): move data location

---
+++ apps/wbtimeline/src/data.ts
@@ -0,0 +1,131 @@
+import type Entry from "@/components/Entry"
+
+const data: Parameters<typeof Entry>[0][] = [
+    {
+        timestamp: "Nov 13, 2017",
+        category: "development",
+        subcategory: "releases",
+        title: "Release of open beta for browsers",
+        description: "",
+    },
+    {
+        timestamp: "Nov 15, 2017",
+        category: "community",
+        subcategory: "videos",
+        description: "",
+        title: "First War Brokers video on YouTube",
+        media: ["https://www.youtube.com/embed/6lPotBirfpU"],
+    },
+    {
+        timestamp: "Jan 18, 2018",
+        category: "development",
+        subcategory: "releases",
+        description: "",
+        title: "Release of open beta on steam",
+    },
+    {
+        timestamp: "Jan 18, 2018",
+        category: "development",
+        subcategory: "game modes",
+        description: "",
+        title: "Pistol only FFA deathmatch mode for the Office map only",
+    },
+    {
+        timestamp: "Feb 12, 2018",
+        category: "development",
+        subcategory: "maps",
+        description: "",
+        title: "Temple",
+    },
+    {
+        timestamp: "Mar 5, 2018",
+        category: "development",
+        subcategory: "weapons",
+        description: "",
+        title: "Minigun & Revolver",
+    },
+    {
+        timestamp: "Mar 13, 2018",
+        category: "development",
+        subcategory: "maps",
+        description: "",
+        title: "Escape",
+    },
+    {
+        timestamp: "Apr 3, 2018",
+        category: "development",
+        subcategory: "stats page",
+        description: "",
+        title: "War Brokers Stats Page goes online",
+    },
+    {
+        timestamp: "Feb 8, 2019",
+        category: "development",
+        subcategory: "weapons",
+        description: "",
+        title: "Crossbow",
+    },
+    {
+        timestamp: "Mar 27, 2019",
+        category: "development",
+        subcategory: "weapons",
+        description: "",
+        title: "SCAR",
+    },
+    {
+        timestamp: "Feb 12, 2020",
+        category: "development",
+        subcategory: "game modes",
+        description: "",
+        title: "Competitive Mode",
+    },
+    {
+        timestamp: "Apr 25, 2020",
+        category: "development",
+        subcategory: "releases",
+        description: "",
+        title: "First picture of WB android build",
+        media: ["/img/android_first.jpg"],
+    },
+    {
+        timestamp: "Apr 26, 2020",
+        category: "community",
+        subcategory: "milestones",
+        description: "",
+        title: "First kill on WB mobile build",
+        media: ["/img/android_first_kill.jpg"],
+    },
+    {
+        timestamp: "May 7, 2020",
+        category: "development",
+        subcategory: "releases",
+        description:
+            "After getting teased by the first image of WB android build on April 25, 2020 and image of first kill on android made on Apr 26, 2020, War Brokers mobile was made available for everyone to enjoy.",
+        title: "Release of open beta for android",
+    },
+    {
+        timestamp: "May 21, 2020",
+        category: "development",
+        subcategory: "maps",
+        description: "",
+        title: "Dungeon",
+    },
+    {
+        timestamp: "Feb 3, 2022",
+        category: "development",
+        subcategory: "updates",
+        description:
+            "<ul>\n<li>Fixed respawn area size for Cyber Tribute.</li>\n<li>Added helicopter to Battle Royale map at Mountain Base.</li>\n</ul>\n",
+        title: "v454",
+    },
+    {
+        timestamp: "Feb 10, 2022",
+        category: "development",
+        subcategory: "updates",
+        description:
+            "<ul>\n<li>Steam users will respawn immediately upon death if possible instead of having to click Respawn button</li>\n<li>Adjust results screen formatting</li>\n</ul>\n",
+        title: "v457",
+    },
+]
+
+export default data
