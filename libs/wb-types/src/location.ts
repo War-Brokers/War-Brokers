0537c6bba0e7bc67ad3a87145ef73bc15f19c8af
developomp <developomp@gmail.com>
2023-08-23 21:03:57 +0900

    refactor(wb-types): add location type

---
+++ libs/wb-types/src/location.ts
@@ -0,0 +1,21 @@
+type Location =
+    | "USA"
+    | "USA_WEST"
+    | "ASIA"
+    | "AUSTRALIA"
+    | "EUROPE"
+    | "INDIA"
+    | "JAPAN"
+    | "RUSSIA"
+    // clan
+    | "USA_CLAN"
+    | "USA_CLAN_WEST"
+    // battle royale
+    | "AS_BATTLE_ROYALE"
+    | "AU_BATTLE_ROYALE"
+    | "EU_BATTLE_ROYALE"
+    | "NA_BATTLE_ROYALE"
+    // competitive
+    | "NA_COMPETITIVE_TESTING"
+
+export default Location
