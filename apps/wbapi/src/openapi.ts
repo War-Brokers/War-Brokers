93a37f551e022dff04c432e1ccfb4c6f30e9839b
developomp <developomp@gmail.com>
2023-08-24 15:35:51 +0900

    fix: build error

--- apps/wbapi/src/openapi.ts
+++ apps/wbapi/src/openapi.ts
@@ -1,18 +1,22 @@
+import type { OpenAPIV3 } from "openapi-types"
 import { generateOpenApiDocument } from "trpc-openapi"
 
 import { appRouter } from "@/router"
 import { tag as pingTag } from "@/router/pingRouter"
 import { tag as playerTag } from "@/router/playersRouter"
 
-export const openApiDocument = generateOpenApiDocument(appRouter, {
-    title: "Example CRUD API",
-    description: "OpenAPI compliant REST API built using tRPC with Express",
-    version: "1.0.0",
-    baseUrl:
-        // eslint-disable-next-line turbo/no-undeclared-env-vars
-        process.env.NODE_ENV === "development"
-            ? "http://localhost:5000"
-            : "https://wbp-wbapi.web.app",
-    docsUrl: "https://github.com/jlalmes/trpc-openapi",
-    tags: [pingTag, playerTag],
-})
+export const openApiDocument: OpenAPIV3.Document = generateOpenApiDocument(
+    appRouter,
+    {
+        title: "Example CRUD API",
+        description: "OpenAPI compliant REST API built using tRPC with Express",
+        version: "1.0.0",
+        baseUrl:
+            // eslint-disable-next-line turbo/no-undeclared-env-vars
+            process.env.NODE_ENV === "development"
+                ? "http://localhost:5000"
+                : "https://wbp-wbapi.web.app",
+        docsUrl: "https://github.com/jlalmes/trpc-openapi",
+        tags: [pingTag, playerTag],
+    },
+)
