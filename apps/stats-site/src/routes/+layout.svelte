a39e6392cade7196fa802f0a6c4e5d49c82dacd9
developomp <developomp@gmail.com>
2023-08-25 00:10:16 +0900

    refactor(stats-site): remove firebase stuff

--- apps/stats-site/src/routes/+layout.svelte
+++ apps/stats-site/src/routes/+layout.svelte
@@ -1,19 +1,14 @@
 <script>
     import "../app.scss"
 
-    import { FirebaseApp } from "sveltefire"
-
     import Footer from "$lib/components/Footer.svelte"
     import Header from "$lib/components/Header.svelte"
-    import { auth, firestore, storage } from "$lib/firebase"
 </script>
 
-<FirebaseApp {auth} {firestore} {storage}>
-    <Header />
+<Header />
 
-    <main class="px-4 flex flex-col my-4 flex-auto max-w-5xl mx-auto w-full">
-        <slot />
-    </main>
+<main class="px-4 flex flex-col my-4 flex-auto max-w-5xl mx-auto w-full">
+    <slot />
+</main>
 
-    <Footer />
-</FirebaseApp>
+<Footer />
