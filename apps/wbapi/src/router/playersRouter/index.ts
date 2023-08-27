a6b6f9beffb74d0f66eb0d017cd543588ae24563
developomp <developomp@gmail.com>
2023-08-27 12:08:36 +0900

    feat(wbapi): add player search API

--- apps/wbapi/src/router/playersRouter/index.ts
+++ apps/wbapi/src/router/playersRouter/index.ts
@@ -1,9 +1,11 @@
 import { createTRPCRouter } from "@/trpc"
 
 import getPlayer from "./getPlayer"
+import searchByName from "./searchByName"
 
 export const tag = "player"
 
 export default createTRPCRouter({
     getPlayer: getPlayer(tag),
+    searchByName: searchByName(tag),
 })
---
+++ apps/wbapi/src/router/playersRouter/searchByName.ts
@@ -0,0 +1,65 @@
+import { TRPCError } from "@trpc/server"
+import { PlayerSchema } from "@warbrokers/types/src/player"
+import { info } from "firebase-functions/logger"
+import fetch from "node-fetch"
+import { z } from "zod"
+
+import { publicProcedure } from "@/trpc"
+
+const searchSchema = z.array(
+    z.tuple([PlayerSchema.shape.nick, PlayerSchema.shape.uid]),
+)
+
+const apiResponseSchema = z.array(
+    z.object({
+        nick: PlayerSchema.shape.nick,
+        uid: PlayerSchema.shape.uid,
+    }),
+)
+
+export default (tag: string) =>
+    publicProcedure
+        .meta({
+            openapi: {
+                method: "GET",
+                path: "/players/searchByName",
+                description: "Search players by their name",
+                tags: [tag],
+            },
+        })
+        .input(
+            z.object({
+                query: z.string(),
+            }),
+        )
+        .output(apiResponseSchema)
+        .query(async ({ input }) => {
+            const { query } = input
+
+            info(`Searching users with ${query}`)
+
+            // todo: replace with firestore full-text search powered by algolia
+            // https://firebase.google.com/docs/firestore/solutions/search?provider=algolia
+            const raw = await fetch(
+                `https://stats.warbrokers.io/players/search?term=${query}`,
+            )
+
+            const a = await raw.json()
+            console.log(a, JSON.stringify(a))
+            const parseResult = searchSchema.safeParse(a)
+            if (!parseResult.success)
+                throw new TRPCError({
+                    code: "INTERNAL_SERVER_ERROR",
+                    message: `Failed to search '${query}'`,
+                })
+
+            const result: z.infer<typeof apiResponseSchema> = []
+            for (const data of parseResult.data) {
+                result.push({
+                    nick: data[0],
+                    uid: data[1],
+                })
+            }
+
+            return result
+        })
