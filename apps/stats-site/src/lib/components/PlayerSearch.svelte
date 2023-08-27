6f71b74085fe6542113991e646fb691c4fe2f7c4
developomp <developomp@gmail.com>
2023-08-27 14:48:14 +0900

    feat(stats-site): implement player search logic

--- apps/stats-site/src/lib/components/PlayerSearch.svelte
+++ apps/stats-site/src/lib/components/PlayerSearch.svelte
@@ -1,24 +1,29 @@
 <script lang="ts">
     import debounce from "lodash/debounce"
 
+    import trpc from "$lib/trpc"
+
     let inputError = ""
+    let searchResult: Awaited<
+        ReturnType<typeof trpc.players.searchByName.query>
+    > = []
 
     const handleSearchInput = debounce(async (e: Event) => {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         const text: string = e.target?.value
 
-        if (!text || text.length < 3) return
+        if (!text) return
 
-        // todo: perform search
+        searchResult = await trpc.players.searchByName.query({ query: text })
     }, 300)
 </script>
 
 <form
     on:submit|preventDefault={() => {}}
-    class="mb-16 mt-6 flex h-14 w-full rounded-full pl-7 dark:bg-gray-600"
+    class="mb-16 mt-6 flex h-14 w-full rounded-full px-7 dark:bg-gray-600"
 >
-    <div class="flex w-full flex-col">
+    <div class="relative flex w-full flex-col">
         <label for="player-search" class="mt-1 text-sm font-black">
             Search
         </label>
@@ -35,6 +40,25 @@
             on:input={handleSearchInput}
         />
         <span class="mt-1 dark:text-red-500">{inputError}</span>
+
+        {#if searchResult.length > 0}
+            <div
+                id="search-results"
+                class="absolute top-20 h-96 max-h-96 w-full overflow-auto rounded-lg py-4 dark:bg-gray-600"
+            >
+                <div class="relative h-full overflow-y-scroll">
+                    {#each searchResult as { nick, uid } (uid)}
+                        <a
+                            href="/players/{uid}"
+                            class="flex w-full flex-col p-2 hover:dark:bg-gray-700"
+                        >
+                            <b class="text-lg">{nick}</b>
+                            <p class="text-md dark:text-gray-400">{uid}</p>
+                        </a>
+                    {/each}
+                </div>
+            </div>
+        {/if}
     </div>
 </form>
 
