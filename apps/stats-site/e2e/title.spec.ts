272154b5c5c32cf4c1f652be02d9940765c8e40f
developomp <developomp@gmail.com>
2023-08-27 17:41:58 +0900

    chore: update tests

--- apps/stats-site/e2e/title.spec.ts
+++ apps/stats-site/e2e/title.spec.ts
@@ -1,11 +1,16 @@
 import { expect, test } from "@playwright/test"
 
-const prefix = "War Brokers Stats | "
+const suffix = " - War Brokers Stats"
 
 test("should have proper title", async ({ page }) => {
     await page.goto("/")
-    expect(await page.title()).toEqual(prefix + "Home")
+    expect(await page.title()).toEqual("Home" + suffix)
+
+    await page.goto("/players/5d2ead35d142affb05757778")
+    expect(await page.title()).toEqual("[LP] POMP" + suffix)
+
+    // todo: add squad page title
 
     await page.goto("/404")
-    expect(await page.title()).toEqual(prefix + "404 Not Found")
+    expect(await page.title()).toEqual("404 Not Found" + suffix)
 })
