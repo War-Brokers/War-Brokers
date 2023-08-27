dafe955c01a76625c71672be74e0423a4260b89b
developomp <developomp@gmail.com>
2023-08-27 18:44:29 +0900

    chore: improve commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,16 @@
+/** @type {import('@commitlint/types').UserConfig} */
 module.exports = {
     extends: ["@commitlint/config-conventional"],
+    rules: {
+        "type-enum": [
+            2,
+            "always",
+            ["chore", "ci", "docs", "feat", "fix", "refactor"],
+        ],
+        "scope-enum": [
+            2,
+            "always",
+            ["bbc", "stats-site", "wbapi", "wbtimeline"],
+        ],
+    },
 }
