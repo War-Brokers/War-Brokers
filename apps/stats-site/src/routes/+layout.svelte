7ba423f7e96cbc9c8f7d809cb0beeba38e748e4f
developomp <developomp@gmail.com>
2023-08-28 21:17:19 +0900

    refactor(stats-site): prevent horizontal scrolling

--- apps/stats-site/src/routes/+layout.svelte
+++ apps/stats-site/src/routes/+layout.svelte
@@ -7,7 +7,9 @@
 
 <Header />
 
-<main class="mx-auto my-4 flex w-full max-w-5xl flex-auto flex-col px-4">
+<main
+    class="mx-auto my-4 flex w-full max-w-5xl flex-auto flex-col overflow-x-hidden px-4"
+>
     <slot />
 </main>
 
