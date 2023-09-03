7708bbd5a2b8a92b46133622dd7fcdb90fa0136c
developomp <developomp@gmail.com>
2023-09-03 21:38:45 +0900

    feat(stats-site): improve home navigation

--- apps/stats-site/src/routes/+page.svelte
+++ apps/stats-site/src/routes/+page.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+    import { A } from "flowbite-svelte"
+    import Icon from "svelte-awesome"
+    import arrowRight from "svelte-awesome/icons/arrowRight"
+
     import PlayerSearch from "$lib/components/PlayerSearch.svelte"
     import Title from "$lib/components/title.svelte"
 
@@ -14,14 +18,18 @@
     <PlayerSearch />
 </div>
 
-<div class="flex flex-wrap gap-10">
+<div class="mb-16 flex flex-wrap gap-10">
     <div>
         <span class="font-bold dark:text-gray-400">Players online</span>
         <h2 class="text-2xl font-black">{playersOnline}</h2>
+        <A href="/servers">Browse Servers &nbsp; <Icon data={arrowRight} /></A>
     </div>
 
     <div>
         <span class="font-bold dark:text-gray-400">Twitch Streams</span>
         <h2 class="text-2xl font-black">{twitchStreams?.total}</h2>
+        <A href="https://www.twitch.tv/warbrokers">
+            Watch Live &nbsp; <Icon data={arrowRight} />
+        </A>
     </div>
 </div>
