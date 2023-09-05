4ab65c3c789f0cb7304b5d008e2f8a60744469c2
developomp <developomp@gmail.com>
2023-09-05 14:00:19 +0900

    feat(wb-types): add map type

---
+++ libs/wb-types/src/map.ts
@@ -0,0 +1,34 @@
+export enum Map {
+    DESERT = 0,
+    NORTHWEST = 1,
+    PACIFIC = 2,
+    OFFICE = 3,
+    FLOODED = 4,
+    TEMPLE = 5,
+    ESCAPE = 6,
+    SOUTHWEST = 7,
+    REMAGEN = 8,
+    // 9
+    // 10
+    // 11
+    // 12
+    CITY_POINT = 13,
+    TOMB = 14,
+    THE_SOMME = 15,
+    // 16
+    // 17
+    TRIBUTE_1 = 18,
+    TRIBUTE_2 = 19,
+    MOONBASE = 20,
+    AREA_15_BASE = 21,
+    AREA_15_BUNKER = 22,
+    // 23
+    SKULL_ISLAND = 24,
+    HANGAR = 25,
+    // 26
+    // 27
+    DWARFS_DUNGEON = 28,
+    KITCHEN = 29,
+    TEST = 30,
+    FRONTIER = 31,
+}
