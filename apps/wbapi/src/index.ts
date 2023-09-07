53f3e8afdd05de8f1ce0ad8588e358e02d2493c9
developomp <developomp@gmail.com>
2023-09-07 17:10:58 +0900

    chore(wbapi): fix lint error

--- apps/wbapi/src/index.ts
+++ apps/wbapi/src/index.ts
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase-admin/app"
 import { setGlobalOptions } from "firebase-functions/v2"
 import { onRequest } from "firebase-functions/v2/https"
-import { onSchedule } from "firebase-functions/v2/scheduler"
 
 import { app } from "@/app"
 import { WB_DOMAIN, WB_ID, WB_PW } from "@/env"
