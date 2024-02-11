4a5e2747de787261c4e28f86efac11f11317bde5
developomp <developomp@protonmail.com>
2024-02-12 00:39:48 +0900

    refactor: migrate stats site to vercel edge (related: #36)

--- apps/stats-site/src/routes/players/[uid]/+page.server.ts
+++ apps/stats-site/src/routes/players/[uid]/+page.server.ts
@@ -1,6 +1,5 @@
 import { error } from "@sveltejs/kit"
 import type { Player } from "@warbrokers/types/src/player"
-import { ObjectId } from "bson"
 import dayjs from "dayjs"
 
 import trpc from "$lib/trpc"
@@ -10,17 +9,21 @@ import type { PageServerLoad } from "./$types"
 export const load = (async ({ params }) => {
     const { uid } = params
 
-    if (!uid) throw error(404, "Not Found")
+    if (!uid) error(404, "Not Found")
 
     try {
         const player: Player = await trpc.players.getPlayer.query({ uid })
         return {
             player,
-            timestamp: dayjs(new ObjectId(player.uid).getTimestamp()).format(
+            timestamp: dayjs(MongoDBObjectId2Date(player.uid)).format(
                 "MMMM D, YYYY",
             ),
         }
     } catch {
-        throw error(404, "Not Found")
+        error(404, "Not Found")
     }
 }) satisfies PageServerLoad
+
+function MongoDBObjectId2Date(s: string) {
+    return new Date(parseInt(s.substring(0, 8), 16) * 1000)
+}
--- apps/stats-site/svelte.config.js
+++ apps/stats-site/svelte.config.js
@@ -1,12 +1,17 @@
-import adapter from "@sveltejs/adapter-node"
-import { vitePreprocess } from "@sveltejs/kit/vite"
+import adapter from "@sveltejs/adapter-vercel"
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+    // https://kit.svelte.dev/docs/integrations#preprocessors
     preprocess: vitePreprocess(),
 
     kit: {
-        adapter: adapter(),
+        // https://kit.svelte.dev/docs/adapters
+        adapter: adapter({
+            runtime: "edge",
+            regions: ["us-west-1", "us-west-2", "us-east-1", "us-east-2"],
+        }),
     },
 }
 
