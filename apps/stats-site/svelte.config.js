1fb7062c48596278fd909c3e50a966a5998115de
developomp <developomp@protonmail.com>
2024-02-12 01:09:32 +0900

    fix: deploy error

--- apps/stats-site/svelte.config.js
+++ apps/stats-site/svelte.config.js
@@ -10,7 +10,15 @@ const config = {
         // https://kit.svelte.dev/docs/adapters
         adapter: adapter({
             runtime: "edge",
-            regions: ["us-west-1", "us-west-2", "us-east-1", "us-east-2"],
+
+            // https://vercel.com/docs/edge-network/regions#region-list
+            regions: [
+                "icn1", // ap-northeast-2 (Seoul, South Korea)
+                "sfo1", // us-west-1 (San Francisco, USA)
+                "pdx1", // us-west-2 (Portland, USA)
+                "iad1", // us-east-1 (Washington, D.C., USA)
+                "cle1", // us-east-2 (Cleveland, USA)
+            ],
         }),
     },
 }
