5b8734fc49ee8d82f6168c4c22956bac153a188e
developomp <developomp@protonmail.com>
2024-02-17 15:21:19 +0900

    refactor(stats-site): update to new domain

--- apps/stats-site/README.md
+++ apps/stats-site/README.md
@@ -1,5 +1,5 @@
 # War Brokers Stats
 
-Live: https://wbp-stats.web.app
+Live: https://stats.wbpjs.com
 
 An attempt to create an improved version of https://stats.warbrokers.io.
--- apps/stats-site/src/lib/trpc.ts
+++ apps/stats-site/src/lib/trpc.ts
@@ -2,5 +2,5 @@ import { createTRPCProxyClient, httpBatchLink } from "@trpc/client"
 import type { AppRouter } from "@warbrokers/wbapi/build/router"
 
 export default createTRPCProxyClient<AppRouter>({
-    links: [httpBatchLink({ url: "https://wbp-wbapi.web.app/trpc" })],
+    links: [httpBatchLink({ url: "https://wbapi.wbpjs.com/trpc" })],
 })
