88f8b68697ced844881b3d5d744a47545675113d
developomp <developomp@protonmail.com>
2024-02-18 19:05:55 +0900

    fix: 403 error in prod

--- apps/stats-site/src/routes/players/[uid]/+page.server.ts
+++ apps/stats-site/src/routes/players/[uid]/+page.server.ts
@@ -1,5 +1,6 @@
 import { error } from "@sveltejs/kit"
-import { getPlayer } from "@warbrokers/fetch/src/players/getPlayer"
+import { FailReason } from "@warbrokers/fetch/src/util/types"
+import { PlayerSchema } from "@warbrokers/types/src/player"
 import dayjs from "dayjs"
 import xss from "xss"
 
@@ -12,14 +13,39 @@ export const load = (async ({ params }) => {
 
     if (!uid) error(404, "Not Found")
 
-    const res = await getPlayer(
-        { id: env.WB_DB_ID, pw: env.WB_DB_PW, ip: env.WB_DB_IP },
-        uid,
+    const res = await fetch(
+        `http://${env.WB_DB_IP}/get_player_stats.php?uid=${uid}`,
+        {
+            method: "GET",
+            headers: {
+                Authorization:
+                    "Basic " +
+                    Buffer.from(`${env.WB_DB_ID}:${env.WB_DB_PW}`).toString(
+                        "base64",
+                    ),
+            },
+        },
     )
 
-    if (!res.success) error(500, res.reason)
+    if (!res.ok) {
+        console.log(
+            `failed to get player stats of ${uid}. DB responded:`,
+            await res.text(),
+        )
+        return error(500, FailReason.DBConnectionFail)
+    }
 
-    const player = res.data
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const raw: any = await res.json()
+
+    // this check works on both null and undefined values because JS
+    if (raw["time_alive_longest"] != null)
+        raw["time_alive_longest"] = Number(raw["time_alive_longest"])
+
+    const parseResult = PlayerSchema.safeParse(raw)
+    if (!parseResult.success) return error(500, FailReason.SchemaValidationFail)
+
+    const player = parseResult.data
 
     // prevent XSS (hopefully)
     player.nick = xss(player.nick)
--- libs/wb-fetch/src/players/getPlayer.ts
+++ libs/wb-fetch/src/players/getPlayer.ts
@@ -2,35 +2,40 @@ import { type Player, PlayerSchema } from "@warbrokers/types/src/player"
 
 import type { DBAuth, Result } from "../util/types"
 import { FailReason } from "../util/types"
-import axios from "axios"
 
 export async function getPlayer(
     auth: DBAuth,
     uid: Player["uid"],
 ): Promise<Result<Player>> {
-    const { id: username, pw: password, ip } = auth
+    const { id, pw, ip } = auth
 
-    const { data, status } = await axios.get<Player>(
-        `http://${ip}/get_player_stats.php?uid=${uid}`,
-        { auth: { username, password } },
-    )
+    const res = await fetch(`http://${ip}/get_player_stats.php?uid=${uid}`, {
+        method: "GET",
+        headers: {
+            Authorization:
+                "Basic " + Buffer.from(`${id}:${pw}`).toString("base64"),
+        },
+    })
 
-    if (status !== 200) {
+    if (!res.ok) {
         console.log(
-            `failed to get player stats of ${uid}. DB responded ${status}`,
+            `failed to get player stats of ${uid}. DB responded:`,
+            await res.text(),
         )
-
         return {
             success: false,
             reason: FailReason.DBConnectionFail,
         }
     }
 
-    // this check works on both null and undefined values because JS
-    if (data["time_alive_longest"] != null)
-        data["time_alive_longest"] = Number(data["time_alive_longest"])
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const raw: any = await res.json()
 
-    const parseResult = PlayerSchema.safeParse(data)
+    // this check works on both null and undefined values because JS
+    if (raw["time_alive_longest"] != null)
+        raw["time_alive_longest"] = Number(raw["time_alive_longest"])
+
+    const parseResult = PlayerSchema.safeParse(raw)
     if (!parseResult.success)
         return {
             success: false,
