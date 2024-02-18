2114c1e9930d3313ae9c19513d702b9bae06a9be
developomp <developomp@protonmail.com>
2024-02-18 18:49:47 +0900

    fix: 403 error in prod

--- libs/wb-fetch/src/players/getPlayer.ts
+++ libs/wb-fetch/src/players/getPlayer.ts
@@ -2,37 +2,35 @@ import { type Player, PlayerSchema } from "@warbrokers/types/src/player"
 
 import type { DBAuth, Result } from "../util/types"
 import { FailReason } from "../util/types"
+import axios from "axios"
 
 export async function getPlayer(
     auth: DBAuth,
     uid: Player["uid"],
 ): Promise<Result<Player>> {
-    const { id, pw, ip } = auth
+    const { id: username, pw: password, ip } = auth
 
-    let headers = new Headers()
-    headers.set(
-        "Authorization",
-        "Basic " + Buffer.from(`${id}:${pw}`).toString("base64"),
+    const { data, status } = await axios.get<Player>(
+        `http://${ip}/get_player_stats.php?uid=${uid}`,
+        { auth: { username, password } },
     )
-    const res = await fetch(`http://${ip}/get_player_stats.php?uid=${uid}`, {
-        credentials: "include",
-        headers: headers,
-    })
 
-    if (!res.ok)
+    if (status !== 200) {
+        console.log(
+            `failed to get player stats of ${uid}. DB responded ${status}`,
+        )
+
         return {
             success: false,
             reason: FailReason.DBConnectionFail,
         }
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const raw: any = await res.json()
+    }
 
     // this check works on both null and undefined values because JS
-    if (raw["time_alive_longest"] != null)
-        raw["time_alive_longest"] = Number(raw["time_alive_longest"])
+    if (data["time_alive_longest"] != null)
+        data["time_alive_longest"] = Number(data["time_alive_longest"])
 
-    const parseResult = PlayerSchema.safeParse(raw)
+    const parseResult = PlayerSchema.safeParse(data)
     if (!parseResult.success)
         return {
             success: false,
