b59fcabf7a387f97d9e82cb98959612435298475
developomp <developomp@protonmail.com>
2024-03-04 20:15:28 +0900

    fix(stats-site): layout shift in ranks page

--- apps/stats-site/src/routes/ranks/RankBadge.svelte
+++ apps/stats-site/src/routes/ranks/RankBadge.svelte
@@ -5,7 +5,12 @@
 </script>
 
 <div class="flex flex-col items-center rounded-xl p-8">
-    <img src={rank2iconMap[rank]} alt="{rank} rank icon" />
+    <img
+        width="256"
+        height="256"
+        src={rank2iconMap[rank]}
+        alt="{rank} rank icon"
+    />
     <h3 class="text-2xl font-black">{rank}</h3>
     <span class="text-lg font-medium dark:text-gray-400">
         better than <span class="font-black">
