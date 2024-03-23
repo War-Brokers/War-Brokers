6fb7b418da171b30ed0bb3d8a7ca1b40603656b1
developomp <developomp@protonmail.com>
2024-03-23 23:47:48 +0900

    refactor: replace vercel analytice with umami

--- apps/stats-site/src/routes/+layout.ts
+++
@@ -1,7 +0,0 @@
-import { inject } from "@vercel/analytics"
-import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit"
-
-import { dev } from "$app/environment"
-
-injectSpeedInsights()
-inject({ mode: dev ? "development" : "production" })
