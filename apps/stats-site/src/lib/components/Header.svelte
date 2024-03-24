5a12d03750fcb13024e707d6cd87035bde5d85e2
developomp <developomp@protonmail.com>
2024-03-25 01:31:43 +0900

    feat: add hamburger menu

--- apps/stats-site/src/lib/components/Header.svelte
+++ apps/stats-site/src/lib/components/Header.svelte
@@ -1,5 +1,18 @@
-<header class="h-16 px-4 dark:bg-gray-700">
-    <div class="mx-auto flex max-w-5xl items-center justify-between">
+<script lang="ts">
+    import { Hamburger } from "svelte-hamburgers"
+
+    let open = false
+    const links = [
+        { name: "Home", path: "/" },
+        { name: "Ranks", path: "/ranks" },
+        { name: "Squads", path: "/squads" },
+    ]
+</script>
+
+<header class="flex flex-col pl-4 md:px-4 dark:bg-gray-700">
+    <div
+        class="mx-auto flex h-16 w-full max-w-5xl items-center justify-between"
+    >
         <div class="flex items-center">
             <img
                 width="64"
@@ -13,11 +26,23 @@
                 </h2>
             </a>
         </div>
-        <div class="flex gap-7">
-            <a href="/" class="hover:text-orange-500">Home</a>
-            <a href="/ranks" class="hover:text-orange-500">Ranks</a>
-            <a href="/squads" class="hover:text-orange-500">Squads</a>
-            <span class="text-gray-400">Leaderboard (WIP)</span>
+
+        <div class="hidden gap-7 md:flex dark:bg-gray-700">
+            {#each links as { name, path }}
+                <a href={path} class="hover:text-orange-500">{name}</a>
+            {/each}
         </div>
+
+        <div class="md:hidden">
+            <Hamburger --color="#eee" bind:open />
+        </div>
+    </div>
+
+    <div
+        class={`${open ? "h-[144px]" : "h-0"} flex flex-col overflow-hidden transition-[height] ease-linear md:hidden dark:bg-gray-700`}
+    >
+        {#each links as { name, path }}
+            <a href={path} class="min-h-[48px] hover:text-orange-500">{name}</a>
+        {/each}
     </div>
 </header>
