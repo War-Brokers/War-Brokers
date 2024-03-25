bba29481045d8ed8320cff7ac044645415900673
developomp <developomp@protonmail.com>
2024-03-25 14:28:50 +0900

    refactor(stats-site): improve error page

--- apps/stats-site/src/routes/+error.svelte
+++ apps/stats-site/src/routes/+error.svelte
@@ -6,5 +6,6 @@
 {#if $page.error}
     <Title title={`${$page.status} ${$page.error.message}`} />
 
-    <h1>{$page.status} - {$page.error.message}</h1>
+    <h1 class="mb-2 text-5xl font-black">{$page.status}</h1>
+    <span class="text-lg">{$page.error.message}</span>
 {/if}
