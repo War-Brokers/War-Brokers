74ffd2ca40bf28b43a1cd77d9727122563568a8f
developomp <developomp@protonmail.com>
2024-04-28 11:29:50 +0900

    feat: proxy umami requests

--- apps/stats-site/vite.config.ts
+++ apps/stats-site/vite.config.ts
@@ -6,4 +6,16 @@ export default defineConfig({
     test: {
         include: ["src/**/*.{test,spec}.{js,ts}"],
     },
+    server: {
+        proxy: {
+            // https://umami.is/docs/guides/bypass-ad-blockers
+            // https://vitejs.dev/config/server-options.html#server-proxy
+            // https://stats.wbpjs.com/stats/* -> https://analytics.umami.is/*
+            "^/stats/.*": {
+                target: "https://analytics.umami.is/",
+                changeOrigin: true,
+                rewrite: (path) => path.slice(6), // slice "/stats" off the string (which is 6 characters long)
+            },
+        },
+    },
 })
