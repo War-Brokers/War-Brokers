e806561159a11c9d568d90dcada988678e27f98b
developomp <developomp@protonmail.com>
2024-05-25 22:56:46 +0900

    feat(stats-site): add custom HF squad page (#62)
    * feat(stats-site): add custom HF squad page
    
    * feat(stats-site): squad leaders in HF squad page

---
+++ apps/stats-site/src/routes/squads/HF/+page.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+    import DiscordIcon from "flowbite-svelte-icons/DiscordSolid.svelte"
+
+    import Title from "$lib/components/title.svelte"
+    import SquadEmblem from "$lib/img/squads/HF.png"
+
+    import type { PageData } from "./$types"
+
+    const title = "HF"
+
+    export let data: PageData
+    const { members, serverMembersCount, squadLeaders } = data
+</script>
+
+<Title {title} />
+
+<img src={SquadEmblem} class="mx-auto w-28" alt="LP squad emblem" />
+<h2 class="mb-4 w-full text-center text-3xl font-black">
+    Squad {title}
+</h2>
+<span class="mx-auto mb-10 text-xl font-black">
+    Squad Leader :
+    <span class="inline-flex gap-1">
+        {#each squadLeaders as player, i (player.uid)}
+            <a
+                class="underline underline-offset-4 hover:text-orange-500"
+                href="/players/{player.uid}"
+            >
+                {player.nick}
+            </a>
+            {#if i < squadLeaders.length - 1}
+                /
+            {/if}
+        {/each}
+    </span>
+</span>
+<a
+    href="https://discord.gg/nZhsAWttge"
+    class="mx-auto flex w-fit items-center justify-center gap-1 underline-offset-4 hover:underline"
+>
+    <DiscordIcon class="inline-block w-9" />
+    Discord (<span><b>{serverMembersCount}</b> members</span>)
+</a>
+
+<div class="mb-4 flex items-baseline gap-1">
+    <span class="text-xl font-bold">{members.length}</span> members
+</div>
+
+<div class="flex flex-col gap-4">
+    {#each members as player (player.uid)}
+        <a href="/players/{player.uid}">
+            <div class="flex flex-col rounded-lg bg-slate-700 p-4">
+                <span class="mb-4 text-xl font-bold">{player.nick}</span>
+
+                <div class="felx flex-row gap-4">Stats (WIP)</div>
+            </div>
+        </a>
+    {/each}
+</div>
