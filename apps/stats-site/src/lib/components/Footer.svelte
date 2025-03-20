a2ecec5eac9ec2ba1eb310941bc895ea2564efc4
pompydev <pompydev@proton.me>
2025-03-20 10:26:08 +0900

    feat(stats-site): add "made by POMP" to footer

--- apps/stats-site/src/lib/components/Footer.svelte
+++ apps/stats-site/src/lib/components/Footer.svelte
@@ -1,13 +1,20 @@
 <script>
     import Icon from "svelte-awesome"
     import github from "svelte-awesome/icons/github"
+
+    import A from "$lib/components/A.svelte"
 </script>
 
-<footer class="w-full dark:bg-gray-950 dark:text-gray-400">
+<footer
+    data-sveltekit-preload-data="hover"
+    class="w-full dark:bg-gray-950 dark:text-gray-400"
+>
     <div
         class="mx-auto flex h-28 w-full max-w-5xl items-center justify-between px-4"
     >
-        <div></div>
+        <div>
+            made by <A href="/players/5d2ead35d142affb05757778">POMP</A>
+        </div>
         <div>
             <a
                 href="https://github.com/War-Brokers/War-Brokers/tree/master/apps/stats-site"
