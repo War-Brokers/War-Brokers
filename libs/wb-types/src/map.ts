2c8caa1222aca237690acb2384d409a8f74e7fc6
EncryptR_ <turtwig.vine@gmail.com>
2025-05-16 15:26:25 +1000

    Maps up to date (to map 49, Gold Mine v2)

--- libs/wb-types/src/map.ts
+++ libs/wb-types/src/map.ts
@@ -10,7 +10,7 @@ export enum WBMap {
     Escape = 6,
     SouthWest = 7,
     Remagen = 8,
-    // 9
+    BattleRoyale = 9,
     // 10
     // 11
     // 12
@@ -18,7 +18,7 @@ export enum WBMap {
     Tomb = 14,
     TheSomme = 15,
     // 16
-    // 17
+    DeadEndCity = 17,
     Tribute = 18,
     CyberTribute = 19,
     Moonbase = 20,
@@ -28,11 +28,30 @@ export enum WBMap {
     SkullIsland = 24,
     Hangar = 25,
     // 26
-    // 27
-    DwarfsDungeon = 28,
+    Quarry = 27,
+    DwarfDungeon = 28,
     Kitchen = 29,
     Test = 30,
     Frontier = 31,
+    Heist = 32,
+    TowerOfPower = 33,
+    ThreeLane = 34,
+    SniperAlley = 35,
+    Pyramid = 36,
+    Containers = 37,
+    SpaceStation = 38,
+    Siege = 39,
+    CrissCross = 40,
+    SnipersOnly = 41,
+    // 42
+    ZenGarden = 43,
+    Cologne = 44,
+    // 45
+    // 46
+    GoldMine = 47,
+    Resort = 48,
+    GoldMineV2 = 49,
+    // 50
 }
 
 export const mapIDSchema = z.nativeEnum(WBMap)
@@ -66,8 +85,27 @@ export const MapName: { [key in WBMap]: string } = {
     [WBMap.Hangar]: "Hangar",
     // 26
     // 27
-    [WBMap.DwarfsDungeon]: "Dwarf's Dungeon",
+    [WBMap.DwarfDungeon]: "Dwarf's Dungeon",
     [WBMap.Kitchen]: "Kitchen",
     [WBMap.Test]: "Test",
     [WBMap.Frontier]: "Frontier",
+    [WBMap.Heist]: "Heist",
+    [WBMap.TowerOfPower]: "Tower of Power",
+    [WBMap.ThreeLane]: "Three Lane",
+    [WBMap.SniperAlley]: "Sniper Alley",
+    [WBMap.Pyramid]: "Pyramid",
+    [WBMap.Containers]: "Containers",
+    [WBMap.SpaceStation]: "Space Station",
+    [WBMap.Siege]: "Siege",
+    [WBMap.CrissCross]: "Criss Cross",
+    [WBMap.SnipersOnly]: "Snipers Only",
+    // 42
+    [WBMap.ZenGarden]: "Zen Garden",
+    [WBMap.Cologne]: "Cologne",
+    // 45
+    // 46
+    [WBMap.GoldMine]: "Gole Mine",
+    [WBMap.Resort]: "Resort",
+    [WBMap.GoldMineV2]: "Gold Mine V2",
+    // 50
 }
