88aca80d0d4e514059a43dd7b2da634758b2d1f1
pompydev <hello@pompy.dev>
2026-08-11 23:06:24 +0900

    stats-site: remove no-longer-necessary config

--- apps/stats-site/svelte.config.js
+++ apps/stats-site/svelte.config.js
@@ -1,20 +1,10 @@
 import adapter from "@sveltejs/adapter-node"
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 
-const vitePreprocessor = vitePreprocess()
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // https://kit.svelte.dev/docs/integrations#preprocessors
-    preprocess: {
-        ...vitePreprocessor,
-        // todo: remove
-        style: async (options) => {
-            if (options.filename?.includes("node_modules/layerchart")) return
-
-            return await vitePreprocessor.style?.(options)
-        },
-    },
+    preprocess: vitePreprocess(),
 
     kit: {
         // https://kit.svelte.dev/docs/adapters
