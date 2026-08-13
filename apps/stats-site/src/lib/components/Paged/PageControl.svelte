bef30cce738b01c0305ac23c1e278aa2e2251a73
pompydev <hello@pompy.dev>
2026-08-13 12:17:53 +0900

    stats-site: run svelte check in `mise run dev`

--- apps/stats-site/src/lib/components/Paged/PageControl.svelte
+++ apps/stats-site/src/lib/components/Paged/PageControl.svelte
@@ -84,7 +84,11 @@
     }
 
     button {
-        @apply flex w-32 items-center justify-center rounded-md bg-slate-500 px-4 py-2 disabled:invisible hover:bg-slate-600 active:bg-slate-700;
+        @apply flex w-32 items-center justify-center rounded-md bg-slate-500 px-4 py-2 hover:bg-slate-600 active:bg-slate-700;
+
+        &:disabled {
+            visibility: hidden;
+        }
     }
 
     b {
